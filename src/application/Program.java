ca6d5b1c29760347bbf43a1f3d3670db65ecd908
Muzashi <evertoncosta100@gmail.com>
2021-08-02 20:43:44 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,12 @@ public class Program {
 		for(Seller s : list) {
 			System.out.println(s);
 		}
+		
+		System.out.println("=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for(Seller s : list) {
+			System.out.println(s);
+		}
 	}
 
 }
